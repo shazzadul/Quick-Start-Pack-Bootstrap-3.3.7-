a78b3f30271300b05fc114c27b106c70dbe3522e
shazzadul <shazzad007@yahoo.com>
2018-05-07 15:09:35 +0600

    Initial commit

---
+++ assets/js/active.js
@@ -0,0 +1,36 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+
+
+
+        /*---
+        * Owl Carousel
+        *-------------------------*/        
+    /*    $(".slider-wrap").owlCarousel({
+            items: 1,
+            nav: true,
+            dots: false,
+            autoplay: true,
+            loop: true,
+            navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+            mouseDrag: true,
+            touchDrag: true,
+        });
+	*/
+		
+		
+		
+    });
+	
+	
+    
+	$(window).load(function(){
+		
+
+		
+	});
+
+})(jQuery);
